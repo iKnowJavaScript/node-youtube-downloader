5948c082eb628d2966ea043c13ab03633e3f8550
iKnowJavaScript <remymartins0398@yahoo.com>
2020-02-16 06:31:21 +0100

    Download video and save to file

---
+++ src/services/queue.ts
@@ -0,0 +1,66 @@
+import Queue from "bull";
+import { Job } from "../entity";
+import ytdl from "ytdl-core";
+import uuidv1 from "uuid/v1";
+import util from "util";
+import fs from "fs";
+
+
+import { customLogger } from "../config/logger";
+import { getConnection } from "typeorm";
+import { STATUS } from "../typings/enum";
+import config from "../config/env";
+import { ExtendedGlobal } from "../typings/extends.interface";
+declare const global: ExtendedGlobal;
+
+const createVideoQueue = (newJob:any) => {
+  const videoQueue = new Queue("video transcoding", {
+    redis: {
+      port: config.REDIS_CONFIG.REDIS_PORT,
+      host: config.REDIS_CONFIG.REDIS_HOST,
+    },
+  });
+  videoQueue.process(async (job: any, done) => {
+    const jobRepository = await getConnection().getRepository(Job);
+
+    const data = job.data;
+    try {
+      job.progress(0);
+      global.io.emit("progress", { progress: 0, jobId: data.id });
+      const uuid = uuidv1();
+      const fileLocation = `./files/${uuid}.mp4`;
+
+      await new Promise(resolve => {
+        ytdl(data.url)
+          .on("progress", (length, downloaded, totallength) => {
+            const progress = (downloaded / totallength) * 100;
+            customLogger.info(`progress`);
+            global.io.emit("progress", { progress, jobId: data.id });
+            if (progress >= 100) {
+              global.io.emit("videoDone", { fileLocation: `${uuid}.mp4`, jobId: data.id });
+              global.io.emit("progress", { progress: 100, jobId: data.id });
+            }
+          })
+          .pipe(fs.createWriteStream(fileLocation))
+          .on("finish", () => {
+            resolve();
+          });
+      });
+      console.log("done");
+      await jobRepository
+        .createQueryBuilder()
+        .update()
+        .set({ status: STATUS.DONE, file_location: `${uuid}.mp4` })
+        .where("id = :id", { id: newJob.id })
+        .execute();
+
+      done();
+    } catch (ex) {
+      console.log(ex, "error");
+      customLogger.error(ex);
+      job.moveToFailed();
+    }
+  });
+  return videoQueue;
+};
+export { createVideoQueue };
